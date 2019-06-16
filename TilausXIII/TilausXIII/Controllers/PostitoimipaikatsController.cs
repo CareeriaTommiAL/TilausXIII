19961b0ca5e321fe4abc39b5ac94eca1885fccdf
TommiAL <tommi.lipponen@student.careeria.fi>
2019-06-16 15:21:35 +0300

    lisättiin kaikki loput TilausDB controllerit CRUD ja login toimintoineen

---
+++ TilausXIII/TilausXIII/Controllers/PostitoimipaikatsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TilausXIII.Models;
+
+namespace TilausXIII.Controllers
+{
+    public class PostitoimipaikatsController : Controller
+    {
+        private TilausDBEntities db = new TilausDBEntities();
+
+        // GET: Postitoimipaikats
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else ViewBag.LoggedStatus = "In";
+            return View(db.Postitoimipaikat.ToList());
+        }     
+
+        // GET: Postitoimipaikats/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Postitoimipaikats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Postinumero,Postitoimipaikka")] Postitoimipaikat postitoimipaikat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Postitoimipaikat.Add(postitoimipaikat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(postitoimipaikat);
+        }
+
+        // GET: Postitoimipaikats/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Postitoimipaikat postitoimipaikat = db.Postitoimipaikat.Find(id);
+            if (postitoimipaikat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postitoimipaikat);
+        }
+
+        // POST: Postitoimipaikats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Postinumero,Postitoimipaikka")] Postitoimipaikat postitoimipaikat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(postitoimipaikat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(postitoimipaikat);
+        }
+
+        // GET: Postitoimipaikats/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Postitoimipaikat postitoimipaikat = db.Postitoimipaikat.Find(id);
+            if (postitoimipaikat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postitoimipaikat);
+        }
+
+        // POST: Postitoimipaikats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Postitoimipaikat postitoimipaikat = db.Postitoimipaikat.Find(id);
+            db.Postitoimipaikat.Remove(postitoimipaikat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
